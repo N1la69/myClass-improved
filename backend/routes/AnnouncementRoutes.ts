e5f12067bb39f606bee28618ad85d78d00088d28
Nilanjan Saha <nilanjan.s.1920@gmail.com>
2025-04-09 13:18:57 +0530

    Admin dashboard - 4 (announcements)

--- backend/routes/AnnouncementRoutes.ts
+++ backend/routes/AnnouncementRoutes.ts
@@ -1,8 +1,12 @@
 import express from "express";
-import { getAllAnnouncements } from "../controllers/AnnouncementController.ts";
+import {
+  getAllAnnouncements,
+  getAnnouncementsByClassId,
+} from "../controllers/AnnouncementController.ts";
 
 const AnnouncementRouter = express.Router();
 
 AnnouncementRouter.get("/", getAllAnnouncements);
+AnnouncementRouter.get("/class/:classId", getAnnouncementsByClassId);
 
 export default AnnouncementRouter;
